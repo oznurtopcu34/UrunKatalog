794efdc109be84fc124880309c59523b6975fc5f
Oznur <oznurtopcu21@gmail.com>
2026-01-22 02:53:39 +0300

    gereksiz kodlar silindi.

--- Onion.Domain/Models/OrderItem.cs
+++ Onion.Domain/Models/OrderItem.cs
@@ -1,10 +1,5 @@
 ﻿using Onion.Domain.Abstract;
 using Onion.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Onion.Domain.Models
 {

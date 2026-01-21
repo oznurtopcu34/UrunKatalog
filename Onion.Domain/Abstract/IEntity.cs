e2888d26e7baed3648f64a71a02d28d423a055e7
Oznur <oznurtopcu21@gmail.com>
2026-01-22 02:47:55 +0300

    Api proje eklendi

--- Onion.Domain/Abstract/IEntity.cs
+++ Onion.Domain/Abstract/IEntity.cs
@@ -1,9 +1,4 @@
 ﻿using Onion.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Onion.Domain.Abstract
 {

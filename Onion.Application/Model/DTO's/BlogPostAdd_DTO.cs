1d099f8978bab83a59c1be4608120d0a3f0dfb36
Oznur <oznurtopcu21@gmail.com>
2026-01-26 19:07:12 +0300

    kod düzenleme

--- Onion.Application/Model/DTO's/BlogPostAdd_DTO.cs
+++ Onion.Application/Model/DTO's/BlogPostAdd_DTO.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Onion.Application.Model.DTO_s
 {
     public class BlogPostAdd_DTO
--- Onion.Application/Model/DTO's/BlogPostUpdate_DTO.cs
+++ Onion.Application/Model/DTO's/BlogPostUpdate_DTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Onion.Application.Model.DTO_s
+﻿namespace Onion.Application.Model.DTO_s
 {
     public class BlogPostUpdate_DTO
     {

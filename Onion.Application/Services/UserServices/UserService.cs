8286f198fc34c7aee5d68b5bafe007eddedc40f6
Oznur <oznurtopcu21@gmail.com>
2026-01-31 21:56:56 +0300

    kod düzenleme

--- Onion.Application/Services/UserServices/UserService.cs
+++ Onion.Application/Services/UserServices/UserService.cs
@@ -2,12 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Onion.Application.Model.DTO_s;
 using Onion.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Onion.Application.Services.UserServices
 {
